e43ec757a0dd0e34a0f71228d1582dc1e7401ada
maulanaakgn <120355453+maulanaakgn@users.noreply.github.com>
2024-05-06 12:31:10 +0700

    Create Hewan.php

---
+++ Hewan.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hewan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hewan';
+    protected $fillable = ['nama', 'jenis'];
+}
+?>
